68d202f2a2d7057406b3962fc8be61f4f71b7391
falkalus <43388421+falkalus@users.noreply.github.com>
2018-10-15 19:40:44 +0000

    fix
    fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/* Búðu til vefsíðu með takka sem virkar þannig 
+að í hvert sinn sem þú ýtir á takkann 
+breytist bakgrunnurinn á síðunni í einhvern lit af handahófi*/
+
 var mainDiv = document.querySelector("#main");
 mainDiv.innerHTML = `
 	<h1>Smelltu á hnappinn:</h1>
--- js/main2.js
+++ js/main2.js
@@ -1,1 +1,12 @@
-/*Búðu til aðra vefsíðu með tveimur <input type=”text”/> reitum og einum takka. Það sem takkinn gerir er að setja inn nýtt div með textanum úr báðum input-unum (svipað og við gerðum með tix (nema bara ekki stolið frá tix í þetta skipti)*/
+/*Búðu til aðra vefsíðu með tveimur <input type=”text”/> 
+reitum og einum takka. 
+Það sem takkinn gerir er að setja inn nýtt div 
+með textanum úr báðum input-unum 
+(svipað og við gerðum með tix*/
+
+var mainDiv = document.querySelector("#wrapper");
+mainDiv.innerHTML = `
+    <br> Name <input type="text"/>  
+    <br> Home <input type="text"/> 
+	<br> <button class="button">add Div</button>
+`
