e3863a41790131c5c3da9898ab46fbcd98ce9553
falkalus <43388421+falkalus@users.noreply.github.com>
2018-10-15 20:17:21 +0000

    done
    done

--- js/main2.js
+++ js/main2.js
@@ -1,4 +1,4 @@
-/*Búðu til aðra vefsíðu með tveimur <input type=”text”/> 
+/*Búðu til vefsíðu með tveimur <input type=”text”/> 
 reitum og einum takka. 
 Það sem takkinn gerir er að setja inn nýtt div 
 með textanum úr báðum input-unum 
@@ -6,7 +6,23 @@ með textanum úr báðum input-unum
 
 var mainDiv = document.querySelector("#wrapper");
 mainDiv.innerHTML = `
-    <br> Name <input type="text"/>  
-    <br> Home <input type="text"/> 
+    <br> Name <input class="name" type="text"/>  
+    <br> Home <input class="home" type="text"/> 
 	<br> <button class="button">add Div</button>
-`
+	<div class="textDiv"></div>
+`;
+var button = document.querySelector(".button");
+var nameInput = document.querySelector(".name");
+var homeInput = document.querySelector(".home");
+var textDiv = document.querySelector(".textDiv");
+
+button.onclick = function(e) {
+	console.log(nameInput.value + homeInput.value);
+	var text = nameInput.value + homeInput.value; 
+	var div = `<div>${text}</div>`;
+	textDiv.innerHTML += div; 
+};
+
+
+
+
